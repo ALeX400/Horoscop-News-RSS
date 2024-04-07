49fc7e736f43d30f8130456795c35ff23616787a
@LeX <ghebaura34@gmail.com>
2024-04-07 13:42:21 +0300

    Update RSS_Horoscop_news.py

--- RSS_Horoscop_news.py
+++ RSS_Horoscop_news.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from lxml import etree
 import os
 from urllib.parse import unquote
+from datetime import datetime
+import pytz
 
 def fetch_article_details(article_url):
     try:
@@ -29,26 +31,51 @@ def fetch_article_details(article_url):
 
 def construct_rss_feed(articles):
     nsmap = {
+        'atom': "http://www.w3.org/2005/Atom",
         'content': "http://purl.org/rss/1.0/modules/content/",
         'wfw': "http://wellformedweb.org/CommentAPI/",
         'dc': "http://purl.org/dc/elements/1.1/",
-        'atom': "http://www.w3.org/2005/Atom",
         'sy': "http://purl.org/rss/1.0/modules/syndication/",
         'slash': "http://purl.org/rss/1.0/modules/slash/"
     }
     rss = etree.Element('rss', nsmap=nsmap, version="2.0")
     channel = etree.SubElement(rss, 'channel')
 
+    # Adăugăm titlul de ansamblu, legătura atom, descrierea și alte detalii la channel
+    title = etree.SubElement(channel, 'title')
+    title.text = "Horoscop Zilnic – Horoscopul zilei de azi & Informatii Zodii"
+
+    atom_link = etree.SubElement(channel, '{http://www.w3.org/2005/Atom}link', href="https://www.horoscop.ro/feed/", rel="self", type="application/rss+xml")
+
+    link = etree.SubElement(channel, 'link')
+    link.text = "https://www.horoscop.ro"
+
+    description = etree.SubElement(channel, 'description')
+    description.text = "Horoscop.ro - Urmărește zilnic horoscopul!"
+
+    # Setăm lastBuildDate la data și ora curentă
+    lastBuildDate = etree.SubElement(channel, 'lastBuildDate')
+    now = datetime.now(pytz.utc)  # Folosim UTC pentru a fi consistent
+    lastBuildDate.text = now.strftime('%a, %d %b %Y %H:%M:%S %z')
+
+    language = etree.SubElement(channel, 'language')
+    language.text = "ro-RO"
+
+    updatePeriod = etree.SubElement(channel, '{http://purl.org/rss/1.0/modules/syndication/}updatePeriod')
+    updatePeriod.text = "hourly"
+
+    updateFrequency = etree.SubElement(channel, '{http://purl.org/rss/1.0/modules/syndication/}updateFrequency')
+    updateFrequency.text = "1"
+
+    # Adăugăm articolele
     for article in articles:
         item = etree.SubElement(channel, 'item')
-        title = etree.SubElement(item, 'title')
-        title.text = article['title']
-        link = etree.SubElement(item, 'link')
-        link.text = article['url']
-        description = etree.SubElement(item, 'description')
-        # Construim CDATA cu URL-ul decodificat și restul conținutului
-        description_content = f'<img src="{article["image_url"]}" /><p>{article["summary"]}</p>{article["content"]}'
-        description.text = etree.CDATA(description_content)
+        item_title = etree.SubElement(item, 'title')
+        item_title.text = article['title']
+        item_link = etree.SubElement(item, 'link')
+        item_link.text = article['url']
+        item_description = etree.SubElement(item, 'description')
+        item_description.text = etree.CDATA(f'<img src="{unquote(article["image_url"])}" /><p>{article["summary"]}</p>{article["content"]}')
 
     return etree.tostring(rss, pretty_print=True, xml_declaration=True, encoding='UTF-8')
 
